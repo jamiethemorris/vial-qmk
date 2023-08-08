fa67547157b7194b144ef058202ba0ac001d8a1f
Jamie Morris <jamiethemorris@gmail.com>
2023-08-07 21:17:47 -0700

    idk

--- keyboards/keychron/q0/rev_0131/keymaps/vial/rules.mk
+++ keyboards/keychron/q0/rev_0131/keymaps/vial/rules.mk
@@ -2,6 +2,12 @@ VIA_ENABLE = yes
 VIAL_ENABLE = yes
 VIALRGB_ENABLE = yes
 ENCODER_MAP_ENABLE = yes
+MIDI_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+DYNAMIC_KEYMAP_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+LEADER_ENABLE = yes
+VIAL_INSECURE = yes
 
 VPATH += keyboards/keychron/common
 SRC += keychron_common.c
