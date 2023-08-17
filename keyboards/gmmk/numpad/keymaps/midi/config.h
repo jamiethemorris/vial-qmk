90e8e52f75115b1e72eec29ce4034337db041d4c
Jamie Morris <jamiethemorris@gmail.com>
2023-08-16 19:44:00 -0700

    qmmk numpad - secure unlock keys

--- keyboards/gmmk/numpad/keymaps/midi/config.h
+++ keyboards/gmmk/numpad/keymaps/midi/config.h
@@ -8,5 +8,6 @@
 
 //Vial Keyboard UID
 #define VIAL_KEYBOARD_UID {0xA8, 0x11, 0x49, 0xEC, 0xFB, 0x28, 0xC1, 0xBE}
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
-#define VIAL_UNLOCK_COMBO_COLS { 1, 2 }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 3 }
+ 
