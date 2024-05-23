c5ebf131aaa1edb0df0b7dae9d79a3aac5db395d
Jamie Morris <jamiethemorris@gmail.com>
2024-05-22 18:55:58 -0700

    drop preonic

---
+++ keyboards/preonic/rev3_drop/keymaps/vial_custom/keymap.c
@@ -0,0 +1,407 @@
+/* Copyright 2015-2021 Jack Humbert
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+#include "muse.h"
+
+enum custom_keycodes {
+	DEBUG = QK_KB_0,
+	RESTART_DEBUG,
+	STOP_DEBUG,
+  KC_TERM,
+  MT_C_CTL_L3,
+};
+
+enum preonic_layers {
+  _DEFAULTS = 0,
+  _MAC = 0,
+  _WIN,
+  _GAME,
+  _LOWER,
+  _RAISE,
+  _ADJUST,
+  _NUMPAD,
+  _MIDI
+};
+
+enum preonic_keycodes {
+  BACKLIT = SAFE_RANGE,
+  LOWER,
+  RAISE
+};
+
+#define LT_SPC_RS LT(_RAISE,KC_SPC) 
+#define LT_SPC_LW LT(_LOWER,KC_SPC)
+#define MT_ENT_GUI MT(MOD_LGUI,KC_ENTER)
+#define MT_ENT_ALT MT(MOD_LALT,KC_ENTER) 
+
+#define MT_F_GUI MT(MOD_LGUI,KC_F)   
+#define MT_D_ALT MT(MOD_LALT,KC_D) 
+#define MT_S_CTL MT(MOD_LCTL,KC_S)
+#define MT_A_SFT MT(MOD_LSFT,KC_A)
+
+#define MT_C_CTL MT(MOD_LCTL,KC_C)
+
+
+#define ST_DBG STOP_DEBUG
+#define RS_DBG RESTART_DEBUG
+#define DB_DBG DEBUG
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* Qwerty
+ * ,-----------------------------------------------------------------------------------.
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Del  |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_MAC] = LAYOUT_preonic_grid(
+  QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    TD(0),
+  TD(1), MT_A_SFT,MT_S_CTL,MT_D_ALT,MT_F_GUI,  KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSFT, KC_Z,    KC_X,    MT_C_CTL,KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
+  KC_LCTL, KC_LCTL, KC_LALT, MT_ENT_GUI,KC_SPC,LT_SPC_LW,KC_SPC, LT_SPC_RS,KC_ENTER,KC_DEL, KC_LBRC, KC_RBRC
+),
+
+[_WIN] = LAYOUT_preonic_grid(
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, KC_LGUI, MT_ENT_ALT,_______,_______,_______, _______, _______, _______, _______, _______
+),
+
+[_GAME] = LAYOUT_preonic_grid(
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, KC_LALT, KC_LALT, _______, KC_SPC,  _______, _______, _______, _______, _______, _______
+),
+
+[_LOWER] = LAYOUT_preonic_grid(
+  KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+  _______, _______, _______, _______, _______, KC_P,    _______, _______, _______, _______, _______, KC_F12,
+  _______, _______, KC_M,    _______, KC_COMM, KC_DOT,  _______, _______, _______, _______, _______, _______,
+  KC_CAPS, _______, KC_BSLS, _______, KC_LCBR, KC_RCBR, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______
+),
+
+[_RAISE] = LAYOUT_preonic_grid(
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,
+  _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_UP,   KC_END,  _______, _______,
+  _______, _______, ST_DBG,  DB_DBG,  RS_DBG,  _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, TO(0),   TG(1),   TG(2)
+),
+
+[_ADJUST] = LAYOUT_preonic_grid(
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+  _______, QK_BOOT, DB_TOGG, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,
+  _______, _______, MU_NEXT, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______, _______, _______, _______, _______,
+  _______, AU_PREV, AU_NEXT, MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+),
+
+[_NUMPAD] = LAYOUT_preonic_grid(
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+),
+
+[_MIDI] = LAYOUT_preonic_grid(
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+),
+
+};
+
+bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LT_SPC_RS:
+        case LT_SPC_LW:
+            // Immediately select the hold action when another key is pressed.                    
+            return true;
+        case MT_ENT_GUI:
+        case MT_ENT_ALT:
+        case MT_F_GUI:
+        case MT_D_ALT:
+        case MT_S_CTL:
+        case MT_A_SFT:
+        case MT_C_CTL:
+             return true;
+        default:
+            // Do not select the hold action when another key is pressed.
+            return false;
+    }
+}
+
+bool get_retro_tapping(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        default:
+            return true;
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+        case LOWER:
+          if (record->event.pressed) {
+            layer_on(_LOWER);
+            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+          } else {
+            layer_off(_LOWER);
+            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+          }
+          return false;
+          break;
+        case RAISE:
+          if (record->event.pressed) {
+            layer_on(_RAISE);
+            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+          } else {
+            layer_off(_RAISE);
+            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+          }
+          return false;
+          break;
+        case BACKLIT:
+          if (record->event.pressed) {
+            register_code(KC_RSFT);
+            #ifdef BACKLIGHT_ENABLE
+              backlight_step();
+            #endif
+            #ifdef RGBLIGHT_ENABLE
+              rgblight_step();
+            #endif
+            #ifdef __AVR__
+            writePinLow(E6);
+            #endif
+          } else {
+            unregister_code(KC_RSFT);
+            #ifdef __AVR__
+            writePinHigh(E6);
+            #endif
+          }
+          return false;
+          break;
+        case MT_C_CTL_L3:
+            if (record->event.pressed) {
+                // Key is pressed
+                register_mods(MOD_BIT(KC_LCTL));
+                layer_on(_LOWER); 
+            } else {
+                // Key is released
+                unregister_mods(MOD_BIT(KC_LCTL));
+                layer_off(_LOWER);
+            }
+            return false;
+        case DEBUG:
+            if (record->event.pressed) {
+                tap_code(KC_F5);
+            }
+            return false;  
+        case RESTART_DEBUG:
+            if (record->event.pressed) {
+                register_code(KC_LGUI);
+                register_code(KC_LSFT);
+                tap_code(KC_F5);
+                unregister_code(KC_LSFT);
+                unregister_code(KC_LGUI);
+            }
+            return false;  
+        case STOP_DEBUG:
+            if (record->event.pressed) {
+                register_code(KC_LSFT);
+                tap_code(KC_F5);
+                unregister_code(KC_LSFT);
+            }
+            return false;  
+        case KC_TERM:
+            if (record->event.pressed) {
+                register_code(KC_LGUI);
+                register_code(KC_LSFT);
+                register_code(KC_LALT);
+                register_code(KC_LCTL);
+                tap_code(KC_T);
+                unregister_code(KC_LGUI);
+                unregister_code(KC_LSFT);
+                unregister_code(KC_LALT);
+                unregister_code(KC_LCTL);
+            }
+            return false;
+      }
+    return true;
+};
+
+bool muse_mode = false;
+uint8_t last_muse_note = 0;
+uint16_t muse_counter = 0;
+uint8_t muse_offset = 70;
+uint16_t muse_tempo = 50;
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+  if (muse_mode) {
+    if (IS_LAYER_ON(_RAISE)) {
+      if (clockwise) {
+        muse_offset++;
+      } else {
+        muse_offset--;
+      }
+    } else {
+      if (clockwise) {
+        muse_tempo+=1;
+      } else {
+        muse_tempo-=1;
+      }
+    }
+  } else {
+    if (clockwise) {
+      register_code(KC_PGDN);
+      unregister_code(KC_PGDN);
+    } else {
+      register_code(KC_PGUP);
+      unregister_code(KC_PGUP);
+    }
+  }
+    return true;
+}
+
+bool dip_switch_update_user(uint8_t index, bool active) {
+    switch (index) {
+        case 0:
+            if (active) {
+                layer_on(_ADJUST);
+            } else {
+                layer_off(_ADJUST);
+            }
+            break;
+        case 1:
+            if (active) {
+                muse_mode = true;
+            } else {
+                muse_mode = false;
+            }
+    }
+    return true;
+}
+
+
+void matrix_scan_user(void) {
+#ifdef AUDIO_ENABLE
+    if (muse_mode) {
+        if (muse_counter == 0) {
+            uint8_t muse_note = muse_offset + SCALE[muse_clock_pulse()];
+            if (muse_note != last_muse_note) {
+                stop_note(compute_freq_for_midi_note(last_muse_note));
+                play_note(compute_freq_for_midi_note(muse_note), 0xF);
+                last_muse_note = muse_note;
+            }
+        }
+        muse_counter = (muse_counter + 1) % muse_tempo;
+    } else {
+        if (muse_counter) {
+            stop_all_notes();
+            muse_counter = 0;
+        }
+    }
+#endif
+}
+
+bool music_mask_user(uint16_t keycode) {
+  switch (keycode) {
+    case RAISE:
+    case LOWER:
+      return false;
+    default:
+      return true;
+  }
+}
+
+void keyboard_post_init_user(void) {
+    vial_tap_dance_entry_t td0 = {KC_MINS,
+                                  KC_EQL,
+                                  KC_NO,
+                                  KC_NO,
+                                  TAPPING_TERM };
+    dynamic_keymap_set_tap_dance(0, &td0);
+    vial_tap_dance_entry_t td1 = {KC_LCTL,
+                                  KC_LCTL,
+                                  KC_TERM,
+                                  OSM(MOD_HYPR),
+                                  TAPPING_TERM };
+    dynamic_keymap_set_tap_dance(1, &td1);
+    vial_tap_dance_entry_t td2 = {KC_NO,
+                                  KC_NO,
+                                  KC_NO,
+                                  KC_NO,
+                                  TAPPING_TERM };
+    dynamic_keymap_set_tap_dance(2, &td2);
+    vial_tap_dance_entry_t td3 = {KC_NO,
+                                  KC_NO,
+                                  KC_NO,
+                                  KC_NO,
+                                  TAPPING_TERM };
+    dynamic_keymap_set_tap_dance(3, &td3);
+    vial_tap_dance_entry_t td4 = { KC_NO,
+                                  KC_NO,
+                                  KC_NO,
+                                  KC_NO,
+                                  TAPPING_TERM };
+    dynamic_keymap_set_tap_dance(4, &td4);
+    vial_tap_dance_entry_t td5 = { KC_NO,
+                                  KC_NO,
+                                  KC_NO,
+                                  KC_NO,
+                                  TAPPING_TERM };
+    dynamic_keymap_set_tap_dance(5, &td5);
+    vial_tap_dance_entry_t td6 = { KC_NO,
+                                  KC_NO,
+                                  KC_NO,
+                                  KC_NO,
+                                  TAPPING_TERM };
+    dynamic_keymap_set_tap_dance(6, &td6);
+    vial_tap_dance_entry_t td7 = { KC_NO,
+                                  KC_NO,
+                                  KC_NO,
+                                  KC_NO,
+                                  TAPPING_TERM };
+    dynamic_keymap_set_tap_dance(7, &td7);
+    vial_tap_dance_entry_t td8 = { KC_NO,
+                                  KC_NO,
+                                  KC_NO,
+                                  KC_NO,
+                                  TAPPING_TERM };
+    dynamic_keymap_set_tap_dance(8, &td8);
+    vial_tap_dance_entry_t td9 = { KC_NO,
+                                  KC_NO,
+                                  KC_NO,
+                                  KC_NO,
+                                  TAPPING_TERM };
+    dynamic_keymap_set_tap_dance(9, &td9);
+}
--- keyboards/preonic/rev3_drop/rules.mk
+++ keyboards/preonic/rev3_drop/rules.mk
@@ -8,13 +8,14 @@ CONSOLE_ENABLE = yes        # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = yes          # Audio output
 CUSTOM_MATRIX = yes
 ENCODER_ENABLE = yes
 DIP_SWITCH_ENABLE = yes
 
 # Do not enable RGB_MATRIX_ENABLE together with RGBLIGHT_ENABLE
-RGB_MATRIX_ENABLE = no
+RGB_MATRIX_ENABLE = yes
+VIALRGB_ENABLE = yes
 
 SRC += matrix.c
